e7024b361d65a44b3445ad6a06cf01ce9db6fd81
michelgithub890 <56154167+michelgithub890@users.noreply.github.com>
2024-01-03 14:32:21 +0100

    0.1.2

--- app/page.js
+++ app/page.js
@@ -1,6 +1,20 @@
-import Image from 'next/image'
+'use client'
 
 export default function Home() {
+  const _handleMap = () => {
+    // window.open(profil[0].map, "_blank")
+    window.open("https://maps.app.goo.gl/Xu1uEKg3HP6XgNi97", "_blank")
+  }
+
+  const _handlePhone = () => {
+      // const phoneUrl = `tel:${profil[0].phone}`
+      const phoneUrl = `tel:7359841296`
+      try {
+          window.location.href = phoneUrl
+      } catch {
+          console.log('impossible')
+      }  
+  }
   return (
     <div className="bg-emerald-100">
       <div>home my-aircut</div>
@@ -12,6 +26,9 @@ export default function Home() {
       <div>home my-aircut</div>
       <div>home my-aircut</div>
       <div>home my-aircut</div>
+      <div className="flex justify-center">
+        <button onClick={_handleMap}>map</button>
+      </div>
       <div>home my-aircut</div>
       <div>home my-aircut</div>
       <div>home my-aircut</div>
@@ -23,7 +40,9 @@ export default function Home() {
       <div>home my-aircut</div>
       <div>home my-aircut</div>
       <div>home my-aircut</div>
-      <div>home my-aircut</div>
+      <div className="flex justify-center">
+        <button onClick={_handlePhone}>phone</button>
+      </div>
       <div>home my-aircut</div>
       <div>home my-aircut</div>
       <div>home my-aircut</div>
