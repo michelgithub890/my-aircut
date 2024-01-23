0f65e347105fffba2fa1746e2c9ef08ed22aabcd
michelgithub890 <56154167+michelgithub890@users.noreply.github.com>
2024-01-23 22:04:03 +0100

    0.5.1

--- pages/api/cron/cron.js
+++ pages/api/cron/cron.js
@@ -1,8 +1,9 @@
-export default async function handler(request, response) {
-  const result = await fetch(
-    'http://worldtimeapi.org/api/timezone/America/Chicago',
-  );
-  const data = await result.json();
- 
-  return response.json({ datetime: data.datetime });
+export default function handler(req, res) {
+  // Ajout d'un console.log pour enregistrer une information dans les logs
+  console.log("La méthode MonAPI a été appelée")
+
+  // Ici, vous pouvez mettre en place votre logique d'API
+
+  // Envoyer une réponse
+  res.status(200).json({ message: 'API appelée avec succès' })
 }
